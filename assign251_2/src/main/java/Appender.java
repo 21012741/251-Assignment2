2ca370379370613e871d24ca529ced775b89dec9
Shihao Zhang <2837991598@qq.com>
2022-11-08 11:59:30 +0800

    1

---
+++ assign251_2/src/main/java/Appender.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MyAppender {
+    public class MapAppender extends AbstractAppender{
+
+
+    }
+
+}
