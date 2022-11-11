0676b38ee25a04c3477032e609a38455f109c510
Shihao Zhang <2837991598@qq.com>
2022-11-11 22:22:05 +0800

    2

---
+++ assign251_2/src/test/java/VelocityLayoutTest.java
@@ -0,0 +1,26 @@
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.Test;
+
+
+class VelocityLayoutTest {
+
+    @Test
+    void format() {
+    }
+
+    @Test
+    void ignoresThrowable() {
+    }
+
+    @Test
+    void activateOptions() {
+    }
+
+    @Test
+    void getPattern() {
+    }
+
+    @Test
+    void setPattern() {
+    }
+}
